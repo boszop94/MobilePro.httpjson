f54743b7d49dfdb50d6e9cab2d794fc91210c145
yossatorn <yossatorn2007@gmail.com>
2017-10-10 11:56:42 +0700

    Initial commit

---
+++ app/src/main/java/egci428/com/httpjson/Helper/Helper.java
@@ -0,0 +1,8 @@
+package egci428.com.httpjson.Helper;
+
+/**
+ * Created by 6272user on 10/10/2017 AD.
+ */
+
+public class Helper {
+}
---
+++ app/src/main/java/egci428/com/httpjson/Models/Quote.kt
@@ -0,0 +1,7 @@
+package egci428.com.httpjson.Models
+
+/**
+ * Created by 6272user on 10/10/2017 AD.
+ */
+class Quote {
+}
